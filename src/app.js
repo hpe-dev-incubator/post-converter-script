a2f19a7b5f301b1a4219accb1f421987ee4b0916
karatehcops <contact@lazers.tv>
2019-10-02 17:14:21 -0400

    Add script to convert portal blog to md.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-import express from 'express';
+import fetch from 'isomorphic-fetch';
+import fs from 'fs';
 
-const app = express();
+const getBlogPost = async slug =>
+  fetch(`https://developer.hpe.com/dashboard/api/post/title/${slug} `)
+    .then(res => res.json())
+    .then(data => data);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const runScript = async () => {
+  const postData = await getBlogPost(
+    'hacktoberfest-2019-help-grommet-and-win-a-free-t-shirt-in-the-process',
+  );
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+  // Convert the post to a markdown string
+  let mdString = `---
+title: ${postData.title}
+date: ${postData.date}
+author: ${postData.author || 'HPE DEV staff'} 
+tags: ${postData.tags.length ? postData.tags : '[]'}
+---
+`;
+
+  for (let i = 0; postData.updatedSections.length > i; i += 1) {
+    const section = postData.updatedSections[i];
+    for (let j = 0; section.contentBlocks.length > j; j += 1) {
+      const currBlock = section.contentBlocks[j];
+      if (currBlock.blockType === 'BlockParagraph') {
+        mdString = `${mdString}${currBlock.content}`;
+      }
+      if (currBlock.blockType === 'BlockImage') {
+        const alt =
+          currBlock.alt ||
+          currBlock.image.title ||
+          currBlock.content ||
+          'blog image';
+        mdString = `${mdString}![${alt}](${currBlock.image.path})\n`;
+      }
+    }
+  }
+
+  // Name and create the file
+  const postDate = new Date(postData.date).toISOString().slice(0, 10);
+  const filename = `${postDate}-${postData.slug.substring(0, 256)}.md`;
+
+  fs.writeFile(filename, mdString, err => {
+    if (err) throw err;
+    console.log(`${filename} has been created successfully.`);
+  });
+};
+
+runScript();
